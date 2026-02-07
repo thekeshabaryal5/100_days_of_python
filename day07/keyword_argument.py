bf09e3eaff3320c3d3aa7b03a0a19ae4f6883b7e
Keshab Aryal <keshabaryal152@gmail.com>
2026-02-07 19:56:52 +0545

    add day07

---
+++ day07/keyword_argument.py
@@ -0,0 +1,7 @@
+def student_info(name, age, course):
+    print("Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+
+# calling function using keyword arguments
+student_info(course="Computer Science", name="Keshab", age=21)
---
+++ day07/positional_arguments.py
@@ -0,0 +1,7 @@
+def student_info(name, age, course):
+    print("Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+
+# calling function using positional arguments
+student_info("Keshab", 21, "Computer Science")
